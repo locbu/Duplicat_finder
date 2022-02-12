4ec5be719c2b8c7b3fdda5d2c46f1536d574d7cd
Dcom-i <den.skirda@gmail.com>
2022-02-12 22:14:17 +0200

    Refactoring code

--- src/main/java/com/duplicate/Main.java
+++ src/main/java/com/duplicate/Main.java
@@ -6,18 +6,30 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
+
     public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
+        GetApp app = new GetApp();
+        var duplicates = app.call(inputAbsolutePath());
+        Set<Map.Entry<String, List<DuplicateItem>>> groupedItems = groupedByHash(duplicates).entrySet();
+        print(groupedItems);
+    }
+
+    private static String inputAbsolutePath() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Please enter the path (as a file system directory) to find a duplicate files - ");
-        String path = scanner.nextLine();
-        GetApp app = new GetApp();
-        var getAllDuplicates = app.CallApp(path);
-        Map<String, List<DuplicateItem>> groupedByHashResult = getAllDuplicates
+        return scanner.nextLine();
+    }
+
+    private static Map<String, List<DuplicateItem>> groupedByHash(Map<String, String> items) {
+        return items
                 .entrySet()
                 .stream()
                 .map(it -> new DuplicateItem(it.getKey(), it.getValue()))
                 .collect(Collectors.groupingBy(DuplicateItem::getHash));
-        for (Map.Entry<String, List<DuplicateItem>> item : groupedByHashResult.entrySet()) {
+    }
+
+    private static void print(Set<Map.Entry<String, List<DuplicateItem>>> items) {
+        for (Map.Entry<String, List<DuplicateItem>> item : items) {
             System.out.println(" This files are fully duplicate ");
             for (DuplicateItem duplicate : item.getValue()) {
                 System.out.println(duplicate.getPath());
