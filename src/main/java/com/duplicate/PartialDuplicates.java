14aa60a4ba8cfaf66608902e7d5c4982a8096e19
dcom-i <den.skyrda@gmail.com>
2024-03-11 01:17:34 +0200

    add method calculateFileHash

--- src/main/java/com/duplicate/PartialDuplicates.java
+++ src/main/java/com/duplicate/PartialDuplicates.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 import java.io.StringReader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class PartialDuplicates {
     public String readTheTextFile(String path) {
@@ -22,6 +24,23 @@ public class PartialDuplicates {
     private void compareFiles(String file1, String file2){
         StringBuilder stringBuilder = new StringBuilder();
 
+    }
+
+    public static String calculateFileHash(String filePath, String hashAlgorithm)
+            throws IOException, NoSuchAlgorithmException {
+        MessageDigest digest = MessageDigest.getInstance(hashAlgorithm);
+        try (FileInputStream fis = new FileInputStream(filePath)) {
+            byte[] byteArray = new byte[8192];
+            int bytesCount;
+            while ((bytesCount = fis.read(byteArray)) != -1) {
+                digest.update(byteArray, 0, bytesCount);
+            }
+        }
+        StringBuilder hashValue = new StringBuilder();
+        for (byte b : digest.digest()) {
+            hashValue.append(String.format("%02x", b));
+        }
+        return hashValue.toString();
     }
 
 
