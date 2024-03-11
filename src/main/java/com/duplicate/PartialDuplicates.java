a4639c339699661fbaa765b1df92e5255f598654
dcom-i <den.skyrda@gmail.com>
2024-03-12 01:18:24 +0200

    add method compareFilesByBytes

--- src/main/java/com/duplicate/PartialDuplicates.java
+++ src/main/java/com/duplicate/PartialDuplicates.java
@@ -8,6 +8,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 public class PartialDuplicates {
     public String readTheTextFile(String path) {
@@ -21,7 +22,7 @@ public class PartialDuplicates {
     }
 
 
-    private void compareFiles(String file1, String file2){
+    private void compareFiles(String file1, String file2) {
         StringBuilder stringBuilder = new StringBuilder();
 
     }
@@ -41,6 +42,25 @@ public class PartialDuplicates {
             hashValue.append(String.format("%02x", b));
         }
         return hashValue.toString();
+    }
+
+    public static boolean compareFilesByBytes(String filePath1, String filePath2) throws IOException {
+        try (FileInputStream fis1 = new FileInputStream(filePath1);
+             FileInputStream fis2 = new FileInputStream(filePath2)) {
+
+            byte[] buffer1 = new byte[8192];
+            byte[] buffer2 = new byte[8192];
+
+            int bytesRead1 = 0;
+            int bytesRead2 = 0;
+
+            while ((bytesRead1 = fis1.read(buffer1)) != -1 && (bytesRead2 = fis2.read(buffer2)) != -1) {
+                if (!Arrays.equals(Arrays.copyOf(buffer1, bytesRead1), Arrays.copyOf(buffer2, bytesRead2))) {
+                    return false;
+                }
+            }
+            return bytesRead1 == -1 && bytesRead2 == -1;
+        }
     }
 
 
